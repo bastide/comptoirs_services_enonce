8654bd8d6eaa553bf4dc7aaf103cd6f71e210126
Rémi Bastide <Remi.Bastide@irit.fr>
2023-02-02 12:58:03 +0100

    Préparation TP

--- src/test/java/comptoirs/dao/CategorieRepositoryTest.java
+++ src/test/java/comptoirs/dao/CategorieRepositoryTest.java
@@ -1,17 +1,26 @@
 package comptoirs.dao;
 
-import comptoirs.entity.Categorie;
-import lombok.extern.log4j.Log4j2;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.jdbc.Sql;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import comptoirs.entity.Categorie;
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2 // Génère le 'logger' pour afficher les messages de trace
 @DataJpaTest
@@ -30,46 +39,63 @@ class CategorieRepositoryTest {
 	}
 
 	@Test
-	void listerLesEntites()  {
+	@Sql("small_data.sql")
+	void listerLesEntites() throws JsonProcessingException {
 		log.info("Lister les entités");
+
 		List<Categorie> liste = categoryDAO.findAll(); // Renvoie la liste des entités dans la table
+
 		assertFalse(liste.isEmpty(), "Il y a des catégories dans le jeu de test");
+
 		log.info("Liste des entités: {}", liste);
 	}
 	
 	@Test
-	void listerCustomQuery() {
+	@Sql("small_data.sql")
+	void listerCustomQuery() throws JsonProcessingException {
 		log.info("Chercher des entités avec une requête 'custom' Spring");
+		
 		String substring = "prod";
+
 		List<Categorie> liste = categoryDAO.findByLibelleContaining(substring);
 		log.info("Entités trouvées: {}", liste);
+		
 		assertEquals(2, liste.size(), "Il y a deux catégories dont le libellé contient la sous-chaine");
+		
 		substring = "xx";
 		assertTrue(categoryDAO.findByLibelleContaining(substring).isEmpty(), "Aucun libellé de catégorie ne contient cette sous-chaine"); 	
 	}
 	
 	@Test
-	void touverParCle()  {
+	@Sql("small_data.sql")
+	void touverParCle() throws JsonProcessingException {
 		log.info("Trouver une entité par sa clé");
+
 		int codePresent = 98;
 		Optional<Categorie> resultat = categoryDAO.findById(codePresent);
-		// On s'assure qu'on a trouvé le résultat
+		// On s'assure qu'on trouvé le résultat
 		assertTrue(resultat.isPresent(), "Cette catégorie existe");
 		Categorie c = resultat.get();
-		assertEquals("7prods", c.getLibelle());
+		assertEquals("2prods", c.getLibelle());
+
 		log.info("Entité trouvée: {}", c);
 	}
 
 	@Test
+	@Sql("small_data.sql")
 	void entiteInconnue()  {
 		log.info("Chercher une entité inconnue");
 		int codeInconnu = 9;
+
 		Optional<Categorie> resultat = categoryDAO.findById(codeInconnu);
+
 		assertFalse(resultat.isPresent(), "Cette catégorie n'existe pas");
+
 	}
 
 	@Test
-	void creerUneEntite()  {
+	@Sql("small_data.sql")
+	void creerUneEntite() throws JsonProcessingException {
 		log.info("Créer une entité");
 		Categorie nouvelle = new Categorie();
 		nouvelle.setLibelle("essai");
@@ -82,12 +108,14 @@ class CategorieRepositoryTest {
 	}
 
 	@Test
-	void modifierEntite()  {
+	@Sql("small_data.sql")
+	void modifierEntite() throws JsonProcessingException {
 		log.info("Modifier une entité");
+
 		int codePresent = 98;
-		String ancienLibelle = "7prods";
+		String ancienLibelle = "2prods";
 		String nouveauLibelle = "Libellé modifié";
-		Categorie c = categoryDAO.findById(codePresent).orElseThrow();
+		Categorie c = categoryDAO.findById(codePresent).get(); 
 		assertEquals(ancienLibelle, c.getLibelle());
 		// On change l'entité
 		c.setLibelle(nouveauLibelle);
@@ -99,27 +127,30 @@ class CategorieRepositoryTest {
 	}
 
 	@Test
+	@Sql("small_data.sql")
 	void erreurCreationEntite() {
 		log.info("Créer une entité avec erreur");
 		Categorie nouvelle = new Categorie();
 		String libelleQuiExiste = "0prod"; // Ce libellé existe dans le jeu de test
 		nouvelle.setLibelle(libelleQuiExiste);  
 		nouvelle.setDescription("essai");
-		try { // L'enregistrement peut générer des exceptions (ex : violation de contrainte d'intégrité)
+		try { // L'enregistreement peut générer des exceptions (ex : violation de contrainte d'intégrité)
 			categoryDAO.save(nouvelle);
 			fail("Les libellés doivent être tous distincts, on doit avoir une exception");
 		} catch (DataIntegrityViolationException e) {
 			// Si on arrive ici c'est normal, on a eu l'exception attendue
 		}
+
 		assertNull(nouvelle.getCode(), "La clé n'a pas été générée, l'entité n'est pas enregistrée");
 	}
 
 	@Test
+	@Sql("small_data.sql")
 	void onNePeutPasDetruireUneCategorieQuiADesProduits() {
 		log.info("Détruire une catégorie avec des produits");
 		int codeCategorieAvecDesProduits = 98;
-		Categorie c = categoryDAO.findById(codeCategorieAvecDesProduits).orElseThrow();
-		assertEquals("7prods", c.getLibelle());
+		Categorie c = categoryDAO.findById(codeCategorieAvecDesProduits).get();
+		assertEquals("2prods", c.getLibelle());
 		// Il y a des produits dans la catégorie 'Boissons'
 		assertFalse(c.getProduits().isEmpty());
 		// Si on essaie de détruire cette catégorie, on doit avoir une exception
