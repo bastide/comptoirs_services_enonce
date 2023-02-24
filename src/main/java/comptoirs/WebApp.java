eb9ffd459d736375d283e76cd83fdcf69b62c5c3
Rémi Bastide <Remi.Bastide@irit.fr>
2023-02-24 11:42:40 +0100

    removed CORS

--- src/main/java/comptoirs/WebApp.java
+++ src/main/java/comptoirs/WebApp.java
@@ -10,25 +10,27 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @SpringBootApplication
 public class WebApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
 
     @Bean
     ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				// registry.addMapping("/api/**").allowedOrigins("http://localhost:5173");
-				registry.addMapping("/comptoirs/**").allowedOrigins("http://localhost:5173");
-                //registry.addMapping("/**").getCo
-			}
-		};
-	}
+    /*
+     * @Bean
+     * public WebMvcConfigurer corsConfigurer() {
+     * return new WebMvcConfigurer() {
+     * 
+     * @Override
+     * public void addCorsMappings(CorsRegistry registry) {
+     * // registry.addMapping("/api/**").allowedOrigins("http://localhost:5173");
+     * registry.addMapping("/comptoirs/**").allowedOrigins("http://localhost:5173");
+     * //registry.addMapping("/**").getCo
+     * }
+     * };
+     * }
+     */
 }
